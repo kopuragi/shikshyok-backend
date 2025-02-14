d47fda13312f460eaabc7e848e725676680787e5
abcbinch <colorquilt112358@gmail.com>
2025-02-14 16:23:56 +0900

    sql

--- SikShokk.sql
+++ SikShokk.sql
@@ -5,7 +5,7 @@ CREATE DATABASE Kopuragi DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general
 #계정명 및 비밀번호 설정
 CREATE USER 'shikshokk'@'%' IDENTIFIED BY '4321';
 
-#계정 사용하기
+#계정 사용하기///
 USE Kopuragi;
 
 #계정에게 권한 부여
