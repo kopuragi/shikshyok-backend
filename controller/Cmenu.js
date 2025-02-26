4ab49787b7a60f967cf2f8f571cb1c30c7a90e07
abcbinch <colorquilt112358@gmail.com>
2025-02-26 18:31:33 +0900

    menu call

--- controller/Cmenu.js
+++ controller/Cmenu.js
@@ -41,7 +41,31 @@ const uploadFile = async (file) => {
 
 exports.getMenus = async (req, res) => {
   console.log("여기는 getMenus");
-  const findMenus = await Menu.findAll();
+  console.log(req.query);
+
+  const { shopId } = req.query;
+  //스타벅스(서브 가게)에 메뉴를 추가하고 나면 shopId가 전달되지 않는다.
+  //좀 더 정확히는, shopId가 텅 비어있는 상태다. undefined
+  //그렇다면, Menus 페이지에 있는 crossId가 undefined가 된다는 뜻.
+  //아마 원인은 이것 같다. useLocation.
+  //저건 처음 선택했을 때만 값을 넘겨준다. 그래서 선택하지 않은 상태에서
+  //메뉴를 불러오는 쿼리를 실행시키려고 하면,
+  //useLocation이 소용없기 때문에, crossId도 텅 빈 값이 되고, shopId도 빈 값이 된다.
+  //shopId를 다른 방법으로 가져오게 해야 할 것 같은데.
+  //그런데 select로 선택한 값을 사용해야 하기는 한다.
+  //shopId를 어디서든 사용할 수 있게 하면서, select에 따라 유동적으로 변하도록
+
+  // const findShop = await Shop.findAll({
+  //   where: {
+  //     owner_id: owner_id,
+  //   },
+  // });
+
+  const findMenus = await Menu.findAll({
+    where: {
+      shop_menu_id: shopId,
+    },
+  });
   console.log("메뉴 조회!");
   res.send(findMenus);
 };
@@ -51,28 +75,30 @@ exports.createMenus = async (req, res) => {
   console.log("이것은 req.body이다.", req.body);
 
   try {
-    const owner_id = Number(req.body.owner_id);
-    console.log("이것은 id다", owner_id);
-    const findShopId = await Shop.findOne({
-      where: {
-        owner_id: owner_id,
-      },
-    });
-    console.log("이것이 findShopId", findShopId);
-    const { id } = findShopId;
-    console.log("shop의 id: ", id);
+    // const owner_id = Number(req.body.owner_id);
+    // console.log("이것은 id다", owner_id);
+    // const findShopId = await Shop.findOne({
+    //   where: {
+    //     owner_id: owner_id,
+    //   },
+    // });
+    // console.log("이것이 findShopId", findShopId);
+    // const { id } = findShopId;
+    // console.log("shop의 id: ", id);
+
+    const shopId = Number(req.body.shopId);
+    console.log("createMenus 안의 ShopId");
 
     const decodeFile = Buffer.from(req.file.originalname, "binary").toString(
       "utf-8"
     );
 
     if (req.file) {
-      console.log("이것은 req.file이다.", req.file);
       const fileUrl = await uploadFile(req.file);
       const s3File = fileUrl.split("/")[3];
       const s3Url = fileUrl.split(s3File)[0];
       const insertMenus = await Menu.create({
-        shop_menu_id: id, //id가 과연...
+        shop_menu_id: shopId, //id가 과연...
         // shop_menu_id: 2, //id가 과연...
         menuName: req.body.mname,
         price: Number(req.body.mprice),
