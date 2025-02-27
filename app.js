e500242c65cfbc2e1e31a0635c3b38c6678a929e
imujinu <wlsdnrhdwkd@gmail.com>
2025-02-27 19:51:28 +0900

    back

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(
 );
 
 const corsOptions = {
-  origin: "http://localhost:3000", // 허용할 도메인
+  origin: ["http://localhost:3000", "http://43.203.244.227"], // 허용할 도메인
   methods: ["GET", "POST", "PUT", "DELETE"], // 허용할 메소드
   credentials: true, // 자격 증명 허용
 };
@@ -80,6 +80,7 @@ const cookingCompletedCustomerStorage = {
     "주문번호가 키임": "12345678-5be4-4fcf-9e9b-d164a3a0ff2d",
   },
 };
+
 const orderApprovedCustomerStorage = {
   onwer: {
     "주문번호가 키임": "12345678-5be4-4fcf-9e9b-d164a3a0ff2d",
--- models/OrderedMenu.js
+++ models/OrderedMenu.js
@@ -2,6 +2,10 @@ const OrderMenu = (sequelize, DataTypes) => {
   return sequelize.define(
     "orderedMenu",
     {
+      shop_order_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
       menuName: {
         type: DataTypes.STRING(255),
         allowNull: true,
